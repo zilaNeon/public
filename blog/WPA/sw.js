ed2333c270928bfa3ec3a25592fb393b6e852f28
zila Neon <106897562+zilaNeon@users.noreply.github.com>
2024-06-17 10:50:14 +0700

    Add files via upload

---
+++ blog/WPA/sw.js
@@ -0,0 +1,17 @@
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('fox-store').then((cache) => cache.addAll([
+      'index.html',
+    ])),
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  console.log(e.request.url);
+
+e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request)),
+  ); 
+
+}); 
+
